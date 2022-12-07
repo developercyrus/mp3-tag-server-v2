c920669049fcd2058dcbdd77571bb0f1f133c5fd
developer cyrus <developer.cyrus@gmail.com>
2022-12-07 17:23:29 +0000

    1st

--- index.js
+++ index.js
@@ -22,15 +22,23 @@ function scan(dir) {
             try {
               id3.parse( data ).then( tags => {
                 var obj = new Object();
+                obj.id = i;
                 obj.filename = files[i];
-                if( Object.keys( tags ).length > 0 ) {
+                if (Object.keys( tags ).length > 0) {
                   //console.log( tags );
                   obj.tags = tags;
+                  if (tags.hasOwnProperty("lyrics") && tags.lyrics) {
+                    obj.lyricshort = tags.lyrics.substring(0, 20) + '...';
+                  }
+                  else {
+                    obj.lyricshort = null
+                  }
                 }
                 else {
                   obj.tags = null;
                 }
                 results.push(obj);
+                console.log(obj);
               });
             }
             catch(e) {
@@ -46,6 +54,8 @@ function scan(dir) {
   });
 }
 
+app.set('view engine', 'ejs');
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -58,9 +68,31 @@ app.get('/api', function (req, res) {
   });
 })
 
+app.get('/api/:id', function (req, res) {
+  r = [];
+  for (let i = 0; i < results.length; i++) {
+    if (results[i].id == req.params.id) {
+      r.push(results[i]);
+      break;
+    }
+  }
+  res.status(200).json({
+    'results': r
+  });
+})
+
+
 //app.get('/', async (req, res) => {
 app.get('/', function (req, res) {
   res.sendFile(path.join(public, 'index.html'));
+});
+
+//app.get('/', async (req, res) => {
+app.get('/view/:id', function (req, res) {
+  
+  res.render('index', {
+    id: req.params.id,
+  });
 });
 
 
